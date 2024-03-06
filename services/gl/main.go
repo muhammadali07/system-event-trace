674380594fd66a3797e26af82a14648fe18fecbc
MAB <muhalibakhtiar@gmail.com>
2024-03-07 00:03:03 +0700

    fixing main.go service gl

--- services/gl/main.go
+++ services/gl/main.go
@@ -5,12 +5,13 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"github.com/muhammadali07/service-grap-go-api/services/gl/api"
+	"github.com/muhammadali07/service-grap-go-api/services/gl/pkg/utils"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
-	"github.com/muhammadali07/service-grap-go-api/services/gl/api"
 )
 
 func main() {
@@ -23,14 +24,16 @@ func main() {
 		panic(err)
 	}
 
-	API_HOST := viper.GetString("API_HOST")
-	API_PORT := viper.GetInt("API_PORT")
+	cfg := utils.Config{}
+
+	API_HOST := cfg.AppHost
+	API_PORT := cfg.AppPort
 	API_ADDRESS := fmt.Sprintf("%s:%d", API_HOST, API_PORT)
-	DB_HOST := viper.GetString("DB_HOST")
-	DB_PORT := viper.GetInt("DB_PORT")
-	DB_USERNAME := viper.GetString("DB_USERNAME")
-	DB_PASSWORD := viper.GetString("DB_PASSWORD")
-	DB_DATABASE := viper.GetString("DB_DATABASE")
+	DB_HOST := cfg.DatabaseHost
+	DB_PORT := cfg.DatabasePort
+	DB_USERNAME := cfg.DatabaseUser
+	DB_PASSWORD := cfg.DatabasePassword
+	DB_DATABASE := cfg.Database
 
 	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Jakarta", DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Silent)})
