4ee5c780e50a92d1c60d92e0b2bda643da26d980
Muh Ali Bakhtiar <muhalibakhtiar@gmail.com>
2024-04-04 05:01:32 +0700

    research graphql

--- services/corporat/main.go
+++ services/corporat/main.go
@@ -1,43 +1,81 @@
 package main
 
 import (
+	"database/sql"
 	"log"
-	"os"
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/joho/godotenv"
-	"github.com/muhammadali07/service-grap-go-api/services/corporat/database"
-	"github.com/muhammadali07/service-grap-go-api/services/corporat/graphql"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gofiber/fiber"
+	"github.com/sqlc-dev/sqlc/postgres"
 )
 
 func main() {
-	// Load environment variables
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	// Initialize database
-	err = database.InitDB()
+	// Koneksi ke database
+	db, err := postgres.Open("postgres://localhost:5432/mydb")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer database.DB.Close()
 
-	// Initialize Fiber
+	// Buat GraphQL server
+	srv := handler.NewDefaultServer(NewExecutableSchema(Config{Resolvers: &Resolver{DB: db}}))
+
+	// Buat Fiber app
 	app := fiber.New()
 
-	// Middleware
-	app.Use(logger.New())
+	// Playground
+	app.Get("/playground", playground.Handler("GraphQL Playground", "/query"))
 
 	// GraphQL endpoint
-	app.Post("/graphql", graphql.GraphqlHandler(database.DB))
+	app.Post("/query", srv.ServeHTTP)
 
-	// Start server
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8000" // Default port
+	// Listen
+	app.Listen(":8080")
+}
+
+type Resolver struct {
+	DB *sql.DB
+}
+
+func (r *Resolver) Users() ([]User, error) {
+	// Query database
+	rows, err := r.DB.Query("SELECT * FROM users")
+	if err != nil {
+		return nil, err
 	}
-	log.Fatal(app.Listen(":" + port))
+	defer rows.Close()
+
+	// Scan rows
+	var users []User
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.Name, &user.Email)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func (r *Resolver) CreateUser(name, email string) (User, error) {
+	// Insert ke database
+	stmt, err := r.DB.Prepare("INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id, name, email")
+	if err != nil {
+		return User{}, err
+	}
+	defer stmt.Close()
+
+	// Jalankan query
+	row := stmt.QueryRow(name, email)
+
+	// Scan hasil
+	var user User
+	err = row.Scan(&user.ID, &user.Name, &user.Email)
+	if err != nil {
+		return User{}, err
+	}
+
+	return user, nil
 }
